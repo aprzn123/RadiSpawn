9610f2cae86101760086cd115dfce07ba49ec669
aprzn <aprzn@aprznpc.hsd1.mi.comcast.net>
2021-11-16 22:25:48 -0500

    Getting closer to a full MVP! Also, some documentation.

--- radial_widget/radial_widget.py
+++ radial_widget/radial_widget.py
@@ -18,26 +18,31 @@ class RadialMenu(QWidget):
 
         self.outer_radius = 200
         self.inner_radius = 100
-        self.select_color = 0x000000
+        self.select_color = 0xffffff
+        self.select_width = 20
         self.wedges = [
                 Wedge("Firefox", 0xff720c, "firefox"),
                 Wedge("Steam", 0x7999e5, "steam"),
-                Wedge("Nautilus", 0xaaaaff, "nautilus")
+                Wedge("Nautilus", 0xffffff, "nautilus")
                 ]
         self.selected_wedge = 0
 
-        self.resize(self.outer_radius * 2, self.outer_radius * 2)
-        self.setMinimumSize(self.outer_radius * 2, self.outer_radius * 2)
+        # Needs to extend to the edge of the border around the outer radius
+        self.resize((self.outer_radius + self.select_width) * 2, (self.outer_radius + self.select_width) * 2)
+        self.setMinimumSize((self.outer_radius + self.select_width) * 2, (self.outer_radius + self.select_width) * 2)
 
+        # Required for keyboard inputs
         self.setEnabled(True)
         self.grabKeyboard()
 
+    # Select the next wedge clockwise
     def nextWedge(self):
         self.selected_wedge -= 1
         if self.selected_wedge < 0:
             self.selected_wedge += len(self.wedges)
         self.update()
     
+    # Select the next wedge counterclockwise
     def prevWedge(self):
         self.selected_wedge += 1
         if self.selected_wedge == len(self.wedges):
@@ -57,7 +62,9 @@ class RadialMenu(QWidget):
             sys.exit(0)
 
     def paintEvent(self, event):
+        # Angle of each wedge
         angle = 360 * 16 / len(self.wedges)
+        # Angle of the right edge of the first wedge
         base_angle = 90 * 16 - angle / 2
 
         painter = QPainter()
@@ -66,14 +73,30 @@ class RadialMenu(QWidget):
         painter.setPen(Qt.NoPen)
         painter.setRenderHint(QPainter.Antialiasing)
 
+        # Variable to save data about the currently selected wedge
+        selection = {}
+        # Draw wedges as pie slices
         painter.setCompositionMode(QPainter.CompositionMode_SourceOver)
         for idx, wedge in enumerate(self.wedges):
-            painter.setBrush(QBrush(wedge.color if idx != self.selected_wedge else 0xffffff))
-            painter.drawPie(0, 0, self.outer_radius * 2, self.outer_radius * 2, base_angle + angle * idx, angle)
-            #painter.setCompositionMode(QPainter.CompositionModeSourceOut)
-            #painter.drawPie(self.outer_radius - self.inner_radius, self.outer_radius - self.inner_radius, self.inner_radius * 2, self.inner_radius * 2, base_angle + angle * idx, angle)
+            painter.setBrush(QBrush(wedge.color))
+            # Extend from a distance from the edge that enables the selection border to be visible
+            painter.drawPie(self.select_width, self.select_width, self.outer_radius * 2, self.outer_radius * 2, base_angle + angle * idx, angle)
+            # Save data about selected wedge
+            if idx == self.selected_wedge:
+                selection['idx'] = idx
+                selection['wedge'] = wedge
 
+        # Erase center of donut
         painter.setCompositionMode(QPainter.CompositionMode_DestinationOut)
-        painter.drawEllipse(self.outer_radius - self.inner_radius, self.outer_radius - self.inner_radius, self.inner_radius * 2, self.inner_radius * 2)
+        # Center is outer + sel_wid, so corners are that +/- inner
+        painter.drawEllipse(self.outer_radius - self.inner_radius + self.select_width, self.outer_radius - self.inner_radius + self.select_width, self.inner_radius * 2, self.inner_radius * 2)
+
+        # Draw border, same logic as the drawPie call above
+        painter.setCompositionMode(QPainter.CompositionMode_SourceOver)
+        pen = QPen(self.select_color)
+        pen.setWidth(self.select_width)
+        pen.setCapStyle(Qt.RoundCap)
+        painter.setPen(pen)
+        painter.drawArc(self.select_width, self.select_width, self.outer_radius * 2, self.outer_radius * 2, base_angle + angle * selection['idx'], angle)
 
         painter.end()
